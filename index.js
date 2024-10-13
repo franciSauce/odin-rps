cef61e3988ea190045e329e82119a03f3789905d
franciSauce <careyfrancisowino@gmail.com>
2024-10-13 13:43:58 +0300

    add playRound function

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ console.log(getHumanChoice());
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(getComputerChoice, getHumanChoice) {
+    if (
+        (getComputerChoice === "rock" && getHumanChoice === "scissors") ||
+        (getComputerChoice === "paper" && getHumanChoice === "rock") ||
+        (getComputerChoice === "scissors" && getHumanChoice === "paper")
+    ) {
+        alert("Comp win!");
+    } else if (getComputerChoice === getHumanChoice) {
+        alert("It's a tie!");
+    } else {
+        alert("You win!");
+    }
+}
+console.log(playRound());
+

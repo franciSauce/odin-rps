e310d414096c0c94f2639556d4be051bec399b54
franciSauce <careyfrancisowino@gmail.com>
2024-10-13 12:23:18 +0300

    add getComputerChoice function

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+function getComputerChoice() {
+    let choices = ["rock","paper","scissors"];
+    let random = Math.floor(Math.random() * 3);
+    return choices[random];
+}
+
+console.log(getComputerChoice());
